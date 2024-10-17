00fde9b8304c64fb32c7a068f2d9d734f06dc747
YeskeAnil <yeske.anil@gmail.com>
2024-10-17 09:59:42 +0530

    Done

---
+++ READM.md
@@ -0,0 +1,9 @@
+Learning Selenium ATB7x
+
+Programs containing selenium learning resources
+
+#TOPICS
+What is selenium?
+Feature of selenium?
+Why selenium advantages and Disadvantages
+Selenium Architecture (W3c, Json wire protocol)
---
+++ src/main/java/org/thetestingacademy/ex_selenium/Sel01.java
@@ -0,0 +1,13 @@
+package org.thetestingacademy.ex_selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sel01 {
+    public static void main(String[] args) {
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://courses.thetestingacademy.com/courses/7x-live-job-ready-automation-tester-blueprint-with-java-by-pramod-dutta/contents/670a50cb95e9f");
+
+    }
+
+}
